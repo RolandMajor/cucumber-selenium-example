8c7b35f74d8b83ad47c67272292ca16a899b0828
RolandMajor <major.sandor@inf.unideb.hu>
2023-04-28 17:35:08 +0200

    Initial commit

---
+++ src/test/java/hu/unideb/inf/RunCucumberTest.java
@@ -0,0 +1,15 @@
+package hu.unideb.inf;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("hu/unideb/inf")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+public class RunCucumberTest {
+}
